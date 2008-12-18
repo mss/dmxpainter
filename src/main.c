33522fa5d7b2f8248b680f6bd5995e370c93b959
Malte S. Stretz <mss@apache.org>
2008-12-18 20:07:35 +0000

    Current state of art.

--- src/main.c
+++ src/main.c
@@ -8,55 +8,28 @@
 
 #include "bits.h"
 
-#define USART_RX_BUFFER_SIZE 64
-#define USART_RX_TIMEOUT     10
+#include "pins.h"
 
-uint8_t          g_usart_rx_buffer[USART_RX_BUFFER_SIZE];
-uint8_t          g_usart_rx_buffer_out;
-volatile uint8_t g_usart_rx_buffer_in;
-volatile uint8_t g_usart_rx_timer;
+#include "tlc.h"
 
-ISR(USART_RX_vect) {
-  uint8_t next;
-  unsigned char data;
-  
-  next = (g_usart_rx_buffer_in + 1) % USART_RX_BUFFER_SIZE;
-  data = UDR;
 
-  // not full?
-  if (next != g_usart_rx_buffer_out) {
-    g_usart_rx_buffer[g_usart_rx_buffer_in] = data;
-	g_usart_rx_buffer_in = next;
-  }
-  else {
-    g_error |= E_USART_BUFFER_OVF;
-  }
-}
-
-/* Each ms */
-ISR(TIMER0_OVF_vect) {
-  TCNT0 = TCNT0_RELOAD;
-
-  if (g_usart_rx_timer)
-    g_usart_rx_timer--;
-}
-
+//////////////////////////////////////////
 
 void main_init() {
   cli();
+
   set_sleep_mode(SLEEP_MODE_IDLE);
 
-  // Configure MISO, MOSI, SCK, SS for SD-access
-  DDRB 
+  tlc_init();
+  tlc_start();
 
-  //sd_raw_init || blink
-
-// init_dmx
+  sei();
 }
 
 void main_loop() {
   while (1) {
-    
+    tlc_update();
+    while (tlc_busy()) {}
   }
 }
 
