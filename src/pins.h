5a056de7370167c2c01979eac0cd71715d2090e7
Malte S. Stretz <mss@apache.org>
2008-12-29 16:05:16 +0000

    Moved some code around.

--- src/pins.h
+++ src/pins.h
@@ -2,15 +2,19 @@
 
 #include "bits.h"
 
-#define _PIN(port, pin)  1 << pin
-#define _PORT(port, pin) PORT ## port
-#define _DDR(port, pin)  DDR  ## port
+
 
 #define pinin(port)  _BC(_DDR(port), _PIN(port))
 #define pinout(port) _BS(_DDR(port), _PIN(port))
 
 #define setpin(port) _BS(_PORT(port), _PIN(port))
 #define clrpin(port) _BC(_PORT(port), _PIN(port))
+
+//////////////////////////////////////////
+
+#define _PIN(port, pin)  1 << pin
+#define _PORT(port, pin) PORT ## port
+#define _DDR(port, pin)  DDR  ## port
 
 //////////////////////////////////////////
 
