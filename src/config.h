3e0e3406806ab9b7d5a016aeedc7b619eebf87d1
Malte S. Stretz <mss@apache.org>
2009-01-06 12:17:34 +0000

    Update pin assignment.

--- src/config.h
+++ src/config.h
@@ -4,12 +4,12 @@
 #define N_PAINTER 1
 #endif
 
-#define PIN_TLC_GSCK  PIN_14
-#define PIN_TLC_XLAT  PIN_28
-#define PIN_TLC_BLNK  PIN_16
-#define PIN_TLC_SCLK  PIN_15
+#define PIN_TLC_GSCK  PIN_15
+#define PIN_TLC_SCLK  PIN_28
+#define PIN_TLC_XLAT  PIN_27
 #define PIN_TLC_SIN   PIN_26
-#define PIN_TLC_VPRG  PIN_23
+#define PIN_TLC_BLNK  PIN_25
+#define PIN_TLC_VPRG  PIN_24
 
 #define PIN_DMX   PIN_INT0
 #define DMX_VECT  INT0_vect
