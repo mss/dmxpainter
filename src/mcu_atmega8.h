a8c84a9839a23fc920cea95ce893bd205c310832
Malte S. Stretz <mss@apache.org>
2009-01-09 10:24:47 +0000

    Doh! Use _BV!

--- src/mcu_atmega8.h
+++ src/mcu_atmega8.h
@@ -8,10 +8,12 @@
 #define mcu_set_timer1_ic(v)   ICR1  = v
 #define mcu_set_timer1_ocma(v) OCR1A = v
 #define mcu_set_timer1_ocmb(v) OCR1B = v
-#define mcu_int_timer1_ocma_enable()  _BS(TIMSK, OCIE1A)
-#define mcu_int_timer1_ocma_disable() _BC(TIMSK, OCIE1A)
-#define mcu_int_timer1_ocmb_enable()  _BS(TIMSK, OCIE1B)
-#define mcu_int_timer1_ocmb_disable() _BC(TIMSK, OCIE1B)
+#define mcu_int_timer1_ovf_enable()   _BS(TIMSK, _BV(TOIE1))
+#define mcu_int_timer1_ovf_disable()  _BC(TIMSK, _BV(TOIE1))
+#define mcu_int_timer1_ocma_enable()  _BS(TIMSK, _BV(OCIE1A))
+#define mcu_int_timer1_ocma_disable() _BC(TIMSK, _BV(OCIE1A))
+#define mcu_int_timer1_ocmb_enable()  _BS(TIMSK, _BV(OCIE1B))
+#define mcu_int_timer1_ocmb_disable() _BC(TIMSK, _BV(OCIE1B))
 #define mcu_pin_timer1_ocma_enable()  pin_out(PIN_OC1A)
 #define mcu_pin_timer1_ocma_disable() pin_in( PIN_OC1A)
 #define mcu_pin_timer1_ocmb_enable()  pin_out(PIN_OC1B)
@@ -19,7 +21,7 @@
 
 #define mcu_set_timer2_cnt(v)  TCNT2 = v
 #define mcu_set_timer2_ic(v)   ICR2  = v
-#define mcu_int_timer2_ocm_enable()   _BS(TIMSK, OCIE2);
-#define mcu_int_timer2_ocm_disable()  _BC(TIMSK, OCIE2);
+#define mcu_int_timer2_ocm_enable()   _BS(TIMSK, _BV(OCIE2));
+#define mcu_int_timer2_ocm_disable()  _BC(TIMSK, _BV(OCIE2));
 #define mcu_pin_timer2_ocm_enable()   pin_out(PIN_OC2)
 #define mcu_pin_timer2_ocm_disable()  pin_in( PIN_OC2)
