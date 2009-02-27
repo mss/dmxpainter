5600a902d360ca0a389c9de430b4b7f900a8820f
Malte S. Stretz <mss@apache.org>
2009-02-27 10:36:25 +0000

    Moved mcu-stuff out of the root.

--- src/mcu.h
+++ src/mcu.h
@@ -7,15 +7,15 @@
 
 #include "pins.h"
 
-#include "mcu_config.h"
+#include "config.h"
 
 #define nop() asm volatile ("nop")
 
 #define mcu_isr(name) ISR(name ## _vect) { INT_ ## name (); } int main(void)
 
 #ifdef __AVR_ATmega8__
-#include "mcu_atmega8.h"
-#include "mcu_atmega8_pins.h"
+#include "mcu/atmega8.h"
+#include "mcu/atmega8_pins.h"
 #else
 #error Unknown MCU
 #endif
