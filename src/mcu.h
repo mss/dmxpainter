94897e55d2249205fe7bae87f484d68365e49ddd
Malte S. Stretz <mss@apache.org>
2009-02-27 11:05:37 +0000

    Conditionally disable mcu_debug_* stuff if PIN_DEBUG is undefined.

--- src/mcu.h
+++ src/mcu.h
@@ -22,10 +22,15 @@
 #endif
 
 
-
+#ifdef PIN_DEBUG
 #define mcu_init() pin_out(PIN_DEBUG)
 volatile uint8_t gg_mcu_debug;
 #define mcu_debug_set(v) do { gg_mcu_debug = v; if (v) { pin_on(PIN_DEBUG); } else { pin_off(PIN_DEBUG); } } while (0)
 #define mcu_debug()      mcu_debug_set(~gg_mcu_debug)
+#else
+#define mcu_init()
+#define mcu_debug_set(v)
+#define mcu_debug()      mcu_debug_set(0xFF)
+#endif
 #define mcu_debug_on()   mcu_debug_set(1)
 #define mcu_debug_off()  mcu_debug_set(0)
