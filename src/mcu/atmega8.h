c1e4c99f663451420c0470fc065ca4237d008e10
Malte S. Stretz <mss@apache.org>
2009-03-13 15:02:44 +0000

    Added getter four the timer counter.

--- src/mcu/atmega8.h
+++ src/mcu/atmega8.h
@@ -3,6 +3,7 @@
 #define mcu_set_timer0_cnt(v) TCNT0 = v
 
 #define mcu_set_timer1_cnt(v)  TCNT1 = v
+#define mcu_get_timer1_cnt()   TCNT1
 #define mcu_set_timer1_ic(v)   ICR1  = v
 #define mcu_set_timer1_ocma(v) OCR1A = v
 #define mcu_set_timer1_ocmb(v) OCR1B = v
@@ -14,6 +15,7 @@
 #define mcu_int_timer1_ocmb_disable() bits_off(TIMSK, OCIE1B)
 
 #define mcu_set_timer2_cnt(v)  TCNT2 = v
+#define mcu_get_timer2_cnt()   TCNT2
 #define mcu_set_timer2_ocm(v)  OCR2  = v
 #define mcu_int_timer2_ocm_enable()   bits_on( TIMSK, OCIE2);
 #define mcu_int_timer2_ocm_disable()  bits_off(TIMSK, OCIE2);
